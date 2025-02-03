7a7453e56f506b7351919ccbad36e8325cf780f9
darknuker101 <dark.nuker101@gmail.com>
2025-02-03 21:40:11 +0200

    Add files via upload

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,9 @@
+const serverURL = "https://gradera.vercel.app/api"
+const hostname = "https://gradera.vercel.app";
+const currencySymbol = "₹";
+const appName = "GraderA";
+//UploadThing
+const maxFileCount = 50;
+const maxFileSize = "8MB"; 
+
+export { serverURL, currencySymbol, appName, maxFileCount, maxFileSize, hostname };
